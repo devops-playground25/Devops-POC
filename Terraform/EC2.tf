19c280314d8af3838af16cbd52a4c9c816074e10
William Kibble <wibblek123@gmail.com>
2025-02-03 19:56:33 +0000

    self signed cert - currently half working

--- Terraform/EC2.tf
+++ Terraform/EC2.tf
@@ -12,9 +12,31 @@ resource "aws_key_pair" "devops_key" {
   public_key = tls_private_key.devops_key.public_key_openssh
 }
 
+resource "tls_self_signed_cert" "self_signed_cert" {
+  private_key_pem = tls_private_key.devops_key.private_key_pem
+
+  subject {
+    common_name  = "yourdomain.com"
+    organization = "Your Organization"
+  }
+
+  validity_period_hours = 8760
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth"
+  ]
+}
+
+resource "aws_iam_server_certificate" "self_signed_cert" {
+  name             = "self-signed-cert"
+  certificate_body = tls_self_signed_cert.cert_pem
+  private_key      = tls_private_key.devops_key.private_key_pem
+}
+
 resource "aws_vpc" "devops_playground" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
@@ -43,20 +65,10 @@ resource "aws_route_table" "devops_route_table" {
   }
 }
 
-resource "aws_route_table_association" "devops_subnet_1_assoc" {
-  subnet_id      = aws_subnet.devops_subnet_1.id
-  route_table_id = aws_route_table.devops_route_table.id
-}
-
-resource "aws_route_table_association" "devops_subnet_2_assoc" {
-  subnet_id      = aws_subnet.devops_subnet_2.id
-  route_table_id = aws_route_table.devops_route_table.id
-}
-
 resource "aws_subnet" "devops_subnet_1" {
-  vpc_id            = aws_vpc.devops_playground.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-west-2a"
+  vpc_id                  = aws_vpc.devops_playground.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-2a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -65,9 +77,9 @@ resource "aws_subnet" "devops_subnet_1" {
 }
 
 resource "aws_subnet" "devops_subnet_2" {
-  vpc_id            = aws_vpc.devops_playground.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "eu-west-2b"
+  vpc_id                  = aws_vpc.devops_playground.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "eu-west-2b"
   map_public_ip_on_launch = true
 
   tags = {
@@ -109,46 +121,11 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-resource "aws_acm_certificate" "web_cert" {
-  domain_name       = "yourdomain.com"  # Replace with your actual domain
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.web_cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  zone_id = "actual-route53-zone-id"
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
-  ttl     = 60
-}
-
-resource "aws_acm_certificate_validation" "web_cert_validation" {
-  certificate_arn         = aws_acm_certificate.web_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
-
-resource "aws_instance" "web" {
-  ami           = "ami-091f18e98bc129c4e"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.devops_key.key_name
-  subnet_id     = aws_subnet.devops_subnet_1.id
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-  tags = {
-    Name = "Terraform-EC2-Instance"
-  }
+resource "aws_lb_target_group" "web_tg" {
+  name     = "web-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.devops_playground.id
 }
 
 resource "aws_lb" "web_lb" {
@@ -158,6 +135,41 @@ resource "aws_lb" "web_lb" {
   security_groups    = [aws_security_group.web_sg.id]
   subnets            = [aws_subnet.devops_subnet_1.id, aws_subnet.devops_subnet_2.id]
   enable_deletion_protection = false
+  depends_on = [aws_lb_target_group.web_tg]
+}
+
+resource "aws_instance" "web" {
+  ami                  = "ami-091f18e98bc129c4e"
+  instance_type        = "t2.micro"
+  key_name             = aws_key_pair.devops_key.key_name
+  subnet_id            = aws_subnet.devops_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "Terraform-EC2-Instance"
+  }
+  depends_on = [aws_lb.web_lb]
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.web_lb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_iam_server_certificate.self_signed_cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web_attach" {
+  target_group_arn = aws_lb_target_group.web_tg.arn
+  target_id        = aws_instance.web.id
+  port             = 80
+
+  depends_on = [aws_lb.web_lb, aws_lb_target_group.web_tg, aws_instance.web]
 }
 
 output "private_key" {
